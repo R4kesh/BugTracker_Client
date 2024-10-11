8acbe03f61b36fd0f895d6a8bdc422a831be5654
rahithkr <rahithkr69@gmail.com>
2024-10-11 13:55:55 +0530

    updatedtestcasetable

---
+++ src/components/AdminDashboard/TestReport.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AdminNavbar from '../AdminNavbar'
+
+function TestReport() {
+  return (
+    <>
+    <div className="bg-black min-h-screen">
+     <AdminNavbar />
+     <UserManageSpotlight/>
+     <Footer />
+   </div>
+  </>
+  )
+}
+
+export default TestReport
