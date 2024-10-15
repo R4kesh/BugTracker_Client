5efedd8c59d620da9a8b3a919fb385ade4fb0115
rahithkr <rahithkr69@gmail.com>
2024-10-15 13:18:58 +0530

    modulelistmodal

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -49,7 +49,7 @@ export const ProjectList = () => {
                 <td className="px-6 py-4">{new Date(project.startDate).toLocaleDateString()}</td>
                 <td className="px-6 py-4 capitalize">{project.status}</td>
                 <td className="px-6 py-4">
-                <Link to={'/projectModule'}> <button
+                <Link to={'/projectModule/id'}> <button
                     className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300"
                     onClick={() => console.log(`Adding task to project ${project.id}`)}
                   >
