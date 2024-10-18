7d09ebf597c15f2234691244866f72b2da176468
rahithkr <rahithkr69@gmail.com>
2024-10-18 17:29:23 +0530

    reassigntable

---
+++ src/components/ReAssignedTaskSpotlight.tsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+import { cn } from "../lib/utils";
+import { Spotlight } from "./ui/spotlight";
+import { TableDemostructure } from "./ui/table";
+import { TableUsermanagement } from "./TableUsermanagement";
+import { SidebarDash } from "./SidebarD";
+import { ReAssignedTaskListTable } from "./ReAssinedTaskListTable";
+
+
+export function ReAssignedTaskSpotlight() {
+  return (
+    <div className="  h-screen  w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <SidebarDash/>
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className="p-4 w-full relative z-10 flex flex-col items-center justify-center">
+          
+        <div className="w-full mb-36  max-w-7xl">
+          {/* <TableDemo />  */}
+         <ReAssignedTaskListTable/>
+        </div>
+      </div>
+    </div>
+  );
+}
