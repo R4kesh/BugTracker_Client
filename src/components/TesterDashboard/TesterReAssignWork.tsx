cad079980bbab1d61f1f223f5ae23a1551b878f4
rahithkr <rahithkr69@gmail.com>
2024-10-18 23:43:01 +0530

    testerreassign

---
+++ src/components/TesterDashboard/TesterReAssignWork.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TesterNavbar from '../TesterNavbar'
+import Footer from '../Footer'
+
+function TesterReAssignWork() {
+  return (
+    <>
+    <div className="bg-black min-h-screen">
+    <TesterNavbar />
+    <TesterShootingStar />
+    <Footer />
+  </div>
+  </>
+  )
+}
+
+export default TesterReAssignWork
